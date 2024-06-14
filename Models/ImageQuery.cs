efc84093ff24350e30a98147e3dd727b114ed73f
whaley <whaley@RS-WHALEY>
2024-06-14 15:14:34 -0700

    Add project files.

---
+++ Models/ImageQuery.cs
@@ -0,0 +1,16 @@
+﻿namespace ImageProjectFrontend.Models
+{
+    /// <summary>
+    /// Model to bind form data to in /Table.
+    /// </summary>
+    public class ImageQuery
+    {
+        public long? Id { get; set; }
+        public string? Name { get; set; } = null!;
+        public string? FilePath { get; set; }
+        public DateTime StartDate { get; set; } = new(new DateOnly(2010, 1, 1), new TimeOnly());
+        public DateTime EndDate { get; set; } = DateTime.UtcNow;
+        public long? UnixTime { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+using ImageProjectFrontend.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddHttpClient();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // See https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
