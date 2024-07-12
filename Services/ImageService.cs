0ce7d310e1f4b5ad0b5c324e9bf7ce4f1002b221
Wyatt Haley <whaley@nshe.nevada.edu>
2024-07-11 17:31:26 -0700

    Add ImageService
    Add ImageService class to manage requests to API related to fetching image data
    
    Add ImageService as a scoped service to IServiceProvider

---
+++ Services/ImageService.cs
@@ -0,0 +1,28 @@
+﻿using ImageProjectFrontend.Models;
+
+namespace ImageProjectFrontend.Services;
+
+public class ImageService(HttpClient httpClient)
+{
+    public async Task<List<Image>> GetImages(DateTime startDate, DateTime endDate, int pageIndex, int pageSize)
+    {
+        var response = await httpClient.GetFromJsonAsync<List<Image>>($"https://localhost:7013/api/images/paginated?startDate={startDate}&endDate={endDate}&pageIndex={pageIndex}&pageSize={pageSize}");
+
+        Console.WriteLine("DEBUG: ImageService.GetImages():");
+        Console.WriteLine();
+        Console.WriteLine(response);
+
+        return response ?? new List<Image>();
+    }
+
+    public string GetImageUri(long Id)
+    {
+        var imageUri = $"https://localhost:7013/api/images/{Id}";
+
+        Console.WriteLine("DEBUG: ImageService.GetImageUri():");
+        Console.WriteLine();
+        Console.WriteLine(imageUri);
+
+        return imageUri;
+    }
+}
