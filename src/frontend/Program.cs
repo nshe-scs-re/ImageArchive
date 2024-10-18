12be5663af3f4b4fb962ec47481e03da1212b510
Wyatt Haley <whaley@nshe.nevada.edu>
2024-10-18 07:34:17 -0700

    Add api request proxying

--- src/frontend/Program.cs
+++ src/frontend/Program.cs
@@ -3,28 +3,59 @@ using frontend.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
 
-builder.Services.AddHttpClient();
+builder.Services.AddHttpClient<ImageService>(client =>
+{
+    var environment = builder.Environment;
+    if(environment.IsDevelopment())
+    {
+        client.BaseAddress = new Uri("https://localhost:8001");
+    }
+    else
+    {
+        client.BaseAddress = new Uri("https://10.176.244.112");
+    }
+});
 
 builder.Services.AddScoped<ImageService>();
 
 var app = builder.Build();
 
-if(!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // See https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+//if(!app.Environment.IsDevelopment())
+//{
+//    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+//    // app.UseHsts(); // TODO: Maybe implement later
+//}
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection(); // HTTPS redirection handled by Nginx in production, unneccessary in dev
 
 app.UseStaticFiles();
+
 app.UseAntiforgery();
 
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
+
+app.MapGet("/api/images/{id}", async (ImageService imageService, long id) =>
+{
+    try
+    {
+
+        var response = await imageService.GetImageByIdAsync(id);
+
+        if(!response.IsSuccessStatusCode)
+        {
+            return Results.NotFound();
+        }
+
+        var stream = await response.Content.ReadAsStreamAsync();
+
+        return Results.Stream(stream, "image/jpeg");
+    }
+    catch(Exception ex)
+    {
+        return Results.Problem($"Error fetching image: {ex.Message}");
+    }
+});
 
 app.Run();
