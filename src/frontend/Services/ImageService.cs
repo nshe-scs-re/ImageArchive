e48e58008af26e5bcab64b38a35c4b1cb9ca1eb4
Wyatt Haley <whaley@nshe.nevada.edu>
2024-10-20 00:04:17 -0700

    Change ImageService constructor for HTTP client configuration

--- src/frontend/Services/ImageService.cs
+++ src/frontend/Services/ImageService.cs
@@ -2,27 +2,40 @@
 
 namespace frontend.Services;
 
-public class ImageService(HttpClient httpClient)
+public class ImageService
 {
+    private readonly HttpClient _httpClient;
+
+    public ImageService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+
+        if(_httpClient.BaseAddress == null)
+        {
+            _httpClient.BaseAddress = new Uri("http://dev_api:8080");
+            //_httpClient.BaseAddress = new Uri("https://dev_api:8081");
+        }
+    }
+
     public async Task<HttpResponseMessage> GetImageByIdAsync(long id)
     {
-        return await httpClient.GetAsync($"/api/images/{id}");
+        return await _httpClient.GetAsync($"/api/images/{id}");
     }
 
     public async Task<List<Image>> GetImages(DateTime startDate, DateTime endDate, int pageIndex, int pageSize)
     {
-        var response = await httpClient.GetFromJsonAsync<List<Image>>($"https://localhost:443/api/images/paginated?startDate={startDate}&endDate={endDate}&pageIndex={pageIndex}&pageSize={pageSize}");
+        var response = await _httpClient.GetFromJsonAsync<List<Image>>($"/api/images/paginated?startDate={startDate}&endDate={endDate}&pageIndex={pageIndex}&pageSize={pageSize}");
 
-        Console.WriteLine($"DEBUG: ImageService.GetImages(): {response.Count()}");
+        //Console.WriteLine($"DEBUG: ImageService.GetImages(): {response.Count()}");
 
         return response ?? new List<Image>();
     }
 
     public string GetImageUri(long Id)
     {
-        var imageUri = $"https://localhost:443/api/images/{Id}";
+        var imageUri = $"/api/images/{Id}";
 
-        Console.WriteLine($"DEBUG: ImageService.GetImageUri(): {imageUri}");
+        //Console.WriteLine($"DEBUG: ImageService.GetImageUri(): {imageUri}");
 
         return imageUri;
     }
