57564c8812c54f20dd7461abfc3f69e886f9a71f
Wyatt Haley <whaley@nshe.nevada.edu>
2024-10-31 17:29:35 -0700

    Add Auth0 support and SDK integration

---
+++ src/frontend/Models/TokenProvider.cs
@@ -0,0 +1,8 @@
+﻿namespace frontend.Models;
+
+public class TokenProvider
+{
+    public string? IdToken { get; set; }
+    public string? AccessToken { get; set; }
+    public string? RefreshToken { get; set; }
+}
