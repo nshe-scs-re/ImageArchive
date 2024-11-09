75885f14090a54e427727b098a9ced539a8fd668
Wyatt Haley <whaley@nshe.nevada.edu>
2024-11-08 17:03:03 -0800

    Update initial values for Image Query Model

--- src/frontend/Models/ImageQuery.cs
+++ src/frontend/Models/ImageQuery.cs
@@ -8,10 +8,10 @@ public class ImageQuery
     public long? Id { get; set; }
     public string? Name { get; set; } = null!;
     public string? FilePath { get; set; }
-    public DateTime StartDateTime { get; set; }
-    public DateTime EndDateTime { get; set; }
-    public TimeOnly StartTimeOnly { get; set; }
-    public TimeOnly EndTimeOnly { get; set; }
+    public DateTime StartDateTime { get; set; } = DateTime.UnixEpoch;
+    public DateTime EndDateTime { get; set; } = DateTime.Now;
+    public TimeOnly StartTimeOnly { get; set; } = TimeOnly.MinValue;
+    public TimeOnly EndTimeOnly { get; set; } = TimeOnly.MaxValue;
     public long? UnixTime { get; set; }
     public string? Site {  get; set; }
     public int? CameraNumber { get; set; }
