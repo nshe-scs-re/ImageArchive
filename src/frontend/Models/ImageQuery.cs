54bc736043ea1bed4255f1ac8ba65c899d887f3a
Wyatt Haley <whaley@nshe.nevada.edu>
2024-11-08 18:08:29 -0800

    Update model initial states

--- src/frontend/Models/ImageQuery.cs
+++ src/frontend/Models/ImageQuery.cs
@@ -6,14 +6,14 @@
 public class ImageQuery
 {
     public long? Id { get; set; }
-    public string? Name { get; set; } = null!;
+    public string? Name { get; set; }
     public string? FilePath { get; set; }
     public DateTime StartDateTime { get; set; } = DateTime.UnixEpoch;
     public DateTime EndDateTime { get; set; } = DateTime.Now;
     public TimeOnly StartTimeOnly { get; set; } = TimeOnly.MinValue;
     public TimeOnly EndTimeOnly { get; set; } = TimeOnly.MaxValue;
     public long? UnixTime { get; set; }
-    public string? Site {  get; set; }
-    public int? CameraNumber { get; set; }
-    public int? CameraPosition { get; set; }
+    public string? Site { get; set; } = "Rockland";
+    public int? CameraNumber { get; set; } = 1;
+    public int? CameraPosition { get; set; } = 1;
 }
