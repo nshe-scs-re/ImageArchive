4527810259c630302b0bbc4e82fb216f394ef2e9
Wyatt Haley <whaley@nshe.nevada.edu>
2025-01-06 15:02:16 -0800

    Add camera positions class to hold position enumerations

---
+++ src/ParseJpegHeaders/Models/CameraPositions.cs
@@ -0,0 +1,29 @@
+﻿namespace ParseJpegHeaders.Models;
+
+public class CameraPositions
+{
+    public enum Rockland
+    {
+
+    }
+
+    public enum Sheep
+    {
+
+    }
+
+    public enum Snake
+    {
+
+    }
+
+    public enum Sagehen
+    {
+
+    }
+
+    public enum Spring
+    {
+
+    }
+}
