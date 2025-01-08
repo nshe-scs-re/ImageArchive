39c2968b09551b6b60148920acc47199cbd3bc56
Wyatt Haley <whaley@nshe.nevada.edu>
2025-01-08 12:55:54 -0800

    Add Entity Framework Core

---
+++ src/ParseJpegHeaders/ImageDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParseJpegHeaders.Models;
+
+namespace ParseJpegHeaders;
+public class ImageDbContext : DbContext
+{
+    public DbSet<Image> Images { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=127.0.0.1,1433;Database=ImageDb;User Id=sa;Password=GZLCS!^S(kx9;Trust Server Certificate=True;");
+    }
+
+}
---
+++ src/ParseJpegHeaders/Models/Image.cs
@@ -0,0 +1,13 @@
+﻿namespace ParseJpegHeaders.Models;
+
+public class Image
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public string FilePath { get; set; }
+    public DateTime DateTime { get; set; }
+    public long UnixTime { get; set; }
+    public string? Site {  get; set; }
+    public int Camera {  get; set; }
+    public int? CameraPosition { get; set; }
+}
