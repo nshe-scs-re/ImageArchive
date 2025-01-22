26170c14b8d3310ea1313ba41c0c7afd41ff21b8
Wyatt Haley <whaley@nshe.nevada.edu>
2025-01-22 10:08:52 -0800

    Add cameraposition parameter to string filter

--- src/frontend/Services/HttpService.cs
+++ src/frontend/Services/HttpService.cs
@@ -47,10 +47,10 @@ public class HttpService
         return await httpClient.GetAsync($"api/images/{id}");
     }
 
-    public async Task<HttpResponseMessage> GetImagesByPageAsync(DateTime startDate, DateTime endDate, int pageIndex, int pageSize, string site)
+    public async Task<HttpResponseMessage> GetImagesByPageAsync(DateTime startDate, DateTime endDate, int pageIndex, int pageSize, string? site, int? cameraPosition)
     {
         var httpClient = CreateClient();
-        return await httpClient.GetAsync($"api/images/paginated?filter={startDate},{endDate},{pageIndex},{pageSize},{site}");
+        return await httpClient.GetAsync($"api/images/paginated?filter={startDate},{endDate},{pageIndex},{pageSize},{site},{cameraPosition}");
     }
 
     public async Task<HttpResponseMessage> GetImagesAllAsync()
