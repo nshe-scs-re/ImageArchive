53e2eae769f2c3d53ae15275c03868eec2b31ebd
Wyatt Haley <whaley@nshe.nevada.edu>
2025-02-20 13:35:20 -0800

    Remove unused fields from Image class

--- src/InitializeDatabase/Image.cs
+++ src/InitializeDatabase/Image.cs
@@ -3,13 +3,11 @@
 public class Image
 {
     public long Id { get; set; }
-    public string? Name { get; set; }
     public string? FilePath { get; set; }
-    public DateTime? DateTime { get; set; }
     public long? UnixTime { get; set; }
+    public DateTime? DateTime { get; set; }
     public string? SiteName {  get; set; }
     public int? SiteNumber { get; set; }
-    public int CameraNumber {  get; set; }
     public int? CameraPositionNumber { get; set; }
     public string? CameraPositionName { get; set; }
 }
