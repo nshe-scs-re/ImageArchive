ec25d418bdf7bd35bb1671389af82b514655d628
Wyatt Haley <whaley@nshe.nevada.edu>
2025-02-19 17:45:11 -0800

    Add image model class

---
+++ src/InitializeDatabase/Image.cs
@@ -0,0 +1,15 @@
+﻿namespace InitializeDatabase;
+
+public class Image
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public string FilePath { get; set; }
+    public DateTime DateTime { get; set; }
+    public long UnixTime { get; set; }
+    public string? SiteName {  get; set; }
+    public int? SiteNumber { get; set; }
+    public int CameraNumber {  get; set; }
+    public int? CameraPositionNumber { get; set; }
+    public string? CameraPositionName { get; set; }
+}
