34077ad44c0f0545c556a3c248b3807731fab0a4
Wyatt Haley <50257740+haley-wyatt@users.noreply.github.com>
2025-02-23 21:46:46 -0800

    Add PostFileAsync

--- src/frontend/Services/HttpService.cs
+++ src/frontend/Services/HttpService.cs
@@ -1,10 +1,9 @@
 ﻿using frontend.Models;
 using Microsoft.AspNetCore.Antiforgery;
-using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Net;
 using System.Globalization;
-namespace frontend.Services; 
+namespace frontend.Services;
 
 public class HttpService
 {
@@ -90,73 +89,118 @@ public class HttpService
             return;
         }
 
-            var tokens = _antiforgery.GetAndStoreTokens(httpContext);
+        var tokens = _antiforgery.GetAndStoreTokens(httpContext);
 
         if (tokens.RequestToken is null || tokens.HeaderName is null)
-            {
+        {
             Console.WriteLine($"[ERROR] [HttpService] [AddAntiForgeryToken]: Antiforgery token fields null.");
         }
 
-                client.DefaultRequestHeaders.Add(tokens.HeaderName, tokens.RequestToken);
+        client.DefaultRequestHeaders.Add(tokens.HeaderName, tokens.RequestToken);
 
         //Console.WriteLine($"[INFO] [HttpService] [AddAntiForgeryToken]: {nameof(tokens.HeaderName)}: {tokens.HeaderName}");
         //Console.WriteLine($"[INFO] [HttpService] [AddAntiForgeryToken]: {nameof(tokens.RequestToken)}: {tokens.RequestToken}");
-            }
-        }
     }
 
     public async Task<HttpResponseMessage> GetImagesByIdAsync(long id)
     {
-        var httpClient = CreateClient();
+        var httpClient = CreateForwardClient();
         return await httpClient.GetAsync($"api/images/{id}");
     }
 
     public async Task<HttpResponseMessage> GetImagesByPageAsync(DateTime startDate, DateTime endDate, int pageIndex, int pageSize, string? siteName, int? siteNumber, int? cameraPosition)
     {
-        var httpClient = CreateClient();
+        var httpClient = CreateForwardClient();
         return await httpClient.GetAsync($"api/images/paginated?filter={startDate},{endDate},{pageIndex},{pageSize},{siteName},{siteNumber},{cameraPosition}");
     }
 
     public async Task<HttpResponseMessage> GetImagesAllAsync()
     {
-        var httpClient = CreateClient();
+        var httpClient = CreateForwardClient();
         return await httpClient.GetAsync("api/images/all");
     }
 
     public async Task<HttpResponseMessage> GetArchiveStatusAsync(Guid jobId)
     {
-        var httpClient = CreateClient();
+        var httpClient = CreateForwardClient();
         return await httpClient.GetAsync($"api/archive/status/{jobId}");
     }
 
     public async Task<HttpResponseMessage> GetArchiveDownloadAsync(Guid jobId)
     {
-        var httpClient = CreateClient();
+        var httpClient = CreateForwardClient();
         return await httpClient.GetAsync($"api/archive/download/{jobId}");
     }
 
     public async Task<HttpResponseMessage> PostArchiveRequestAsync(ArchiveRequest request)
     {
-        var httpClient = CreateClient();
+        var httpClient = CreateForwardClient();
         return await httpClient.PostAsJsonAsync("api/archive/request", request);
     }
 
-    public async Task<HttpResponseMessage> UploadImageAsync(List<IBrowserFile> files)
+    public async Task<HttpResponseMessage> PostFileAsync(FileUploadItem item)
     {
-        var httpClient = CreateClient();
+        var httpClient = CreateForwardClient();
 
         using var content = new MultipartFormDataContent();
-        foreach(var file in files)
+
+        var fileContent = new StreamContent(item.File.OpenReadStream(maxAllowedSize: 10 * 1024 * 1024));
+
+        fileContent.Headers.ContentType = new MediaTypeHeaderValue(item.File.ContentType);
+
+        content.Add(fileContent, "files", item.File.Name);
+
+        if(item.DateTime.HasValue)
         {
-            var fileContent = new StreamContent(file.OpenReadStream(maxAllowedSize: 10 * 1024 * 1024)); // 10 MB max file size
-            fileContent.Headers.ContentType = new MediaTypeHeaderValue(file.ContentType);
-            content.Add(fileContent, "files", file.Name);
+            content.Add(new StringContent(item.DateTime.Value.ToString("o")), "DateTime");
         }
 
-        var response = await httpClient.PostAsync("/api/upload/multiple", content);
-        response.EnsureSuccessStatusCode();
-        return response;
+        if(!string.IsNullOrEmpty(item.SiteName))
+        {
+            content.Add(new StringContent(item.SiteName), "SiteName");
+        }
+
+        if(item.SiteNumber.HasValue)
+        {
+            content.Add(new StringContent(item.SiteNumber.Value.ToString(CultureInfo.InvariantCulture)), "SiteNumber");
+        }
+
+        if(item.CameraPositionNumber.HasValue)
+        {
+            content.Add(new StringContent(item.CameraPositionNumber.Value.ToString(CultureInfo.InvariantCulture)), "CameraPositionNumber");
+        }
+
+        if(!string.IsNullOrEmpty(item.CameraPositionName))
+        {
+            content.Add(new StringContent(item.CameraPositionName), "CameraPositionName");
+        }
+
+        return await httpClient.PostAsync("api/upload", content);
     }
 
+    public async Task<HttpResponseMessage> UploadFilesAsync(List<FileUploadItem> fileItems)
+    {
+        var httpClient = CreateForwardClient();
 
+        using var content = new MultipartFormDataContent();
+
+        for(int i=0; i < fileItems.Count; i++)
+        {
+            var item = fileItems[i];
+
+            var fileContent = new StreamContent(item.File.OpenReadStream(maxAllowedSize: 10 * 1024 * 1024)); // 10 MB max file size
+
+            fileContent.Headers.ContentType = new MediaTypeHeaderValue(item.File.ContentType);
+
+            content.Add(fileContent, "files", item.File.Name);
+
+            //content.Add(new StringContent(item.DateTime.Value.ToString("o")), $"FileItems[{i}].DateTime");
+            //content.Add(new StringContent(item.SiteName), $"FileItems[{i}].SiteName");
+            //content.Add(new StringContent(item.SiteNumber.ToString()), $"FileItems[{i}].SiteNumber");
+            //content.Add(new StringContent(item.CameraPositionNumber.ToString()), $"FileItems[{i}].CameraPositionNumber");
+            //content.Add(new StringContent(item.CameraPositionName), $"FileItems[{i}].CameraPositionName");
+        }
+
+        return await httpClient.PostAsync("/api/upload", content);
+    }
 }
