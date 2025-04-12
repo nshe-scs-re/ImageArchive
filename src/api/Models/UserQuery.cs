81fd64554119e96cc6c5efd5910c2e1c60d735b0
Di-Diana-AJ-1103 <adidiana03@gmail.com>
2025-04-11 22:48:15 -0700

    stop with the initializedatabase.models

--- src/api/Models/UserQuery.cs
+++ src/api/Models/UserQuery.cs
@@ -2,7 +2,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace InitializeDatabase.Models
+namespace api.Models
 {
     public class UserQuery
     {
