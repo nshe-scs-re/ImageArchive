0ffa3d556e1ebbb583726d9275dfbf0544bde938
Wyatt Haley <wyattthaley@gmail.com>
2025-04-19 20:46:28 -0700

    Add ImageQuery class to backend

---
+++ src/api/Models/ImageQuery.cs
@@ -0,0 +1,17 @@
+﻿namespace api.Models;
+
+/// <summary>
+/// Model to bind form data.
+/// </summary>
+public class ImageQuery
+{
+    public DateTime? StartDateTime { get; set; }
+    public DateTime? EndDateTime { get; set; }
+    public string? SiteName { get; set; }
+    public int? SiteNumber { get; set; }
+    public int? CameraPositionNumber { get; set; }
+    public string? WeatherPrediction { get; set; }
+    public double? WeatherPredictionPercent { get; set; }
+    public string? SnowPrediction { get; set; }
+    public double? SnowPredictionPercent { get; set; }
+}
