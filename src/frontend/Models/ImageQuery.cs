d39a9fc4b27705caec804b31a738286be6bf02e9
Wyatt Haley <wyattthaley@gmail.com>
2025-04-19 20:46:53 -0700

    Update ImageQuery class

--- src/frontend/Models/ImageQuery.cs
+++ src/frontend/Models/ImageQuery.cs
@@ -5,9 +5,13 @@
 /// </summary>
 public class ImageQuery
 {
-    public DateTime StartDateTime { get; set; }
-    public DateTime EndDateTime { get; set; }
+    public DateTime? StartDateTime { get; set; }
+    public DateTime? EndDateTime { get; set; }
     public string? SiteName { get; set; }
-    public int SiteNumber { get; set; }
-    public int CameraPositionNumber { get; set; }
+    public int? SiteNumber { get; set; }
+    public int? CameraPositionNumber { get; set; }
+    public string? WeatherPrediction { get; set; }
+    public double? WeatherPredictionPercent { get; set; }
+    public string? SnowPrediction { get; set; }
+    public double? SnowPredictionPercent { get; set; }
 }
